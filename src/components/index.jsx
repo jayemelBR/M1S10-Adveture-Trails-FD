764557365b646d3c40d113f6040c174982316c41
Jorge Luz <135906914+jayemelBR@users.noreply.github.com>
2024-04-08 23:36:41 -0300

    Update index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,6 @@
 import * as PropTypes from 'prop-types'
 import "./style.css"
+import { useState, useEffect } from 'react';
 
 function CardTrilha(dadosTrilha){
     return (
@@ -10,7 +11,60 @@ function CardTrilha(dadosTrilha){
         </div>
     )
 }
+const MinhaTelaDeTrilhas = () => {
+    const { data, isLoading, error } = useFetch('./public/CardTrilha.json');
+    if (isLoading) {
+        return <div>Loading...</div>;
+      }
+    
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      }
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          {/* Renderiza os cards*/}
+          <CardTrilha title="Trilha da costa da Lagoa" description="Descrição da Trilha 1" imageUrl="https://images.pexels.com/photos/18078207/pexels-photo-18078207/free-photo-of-forest-trail.png" />
+        </div>
+      </div>
+    );
+  };
 
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        setIsLoading(true);
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error('Failed to fetch data');
+          }
+          const result = await response.json();
+          setData(result);
+          setError(null);
+        } catch (error) {
+          setError(error);
+          setData(null);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+  
+      fetchData();
+  
+      // Cleanup function to abort fetch if component unmounts before completion
+      return () => {
+        controller.abort();
+      };
+    }, [url]);
+  
+    return { data, isLoading, error };
+  };
 //configuração de Prop-Types
 CardTrilha.propTypes = {
     dadosTrilha: PropTypes.exact({
